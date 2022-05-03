44f278c5907d9aa8afa93d9472db0898ed481288
nelsoneo <nelsoneo@gmail.com>
2022-05-03 12:12:58 -0300

    ajust

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,59 +1,153 @@
-let inputNovaTarefa = document.querySelector("#inputNovaTarefa");
-let btnAddTarefa = document.querySelector("#btnAddTarefa");
-let listaTarefa = document.querySelector("#listaTarefa");
+let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
+let btnAddTarefa = document.querySelector('#btnAddTarefa');
+let listaTarefas = document.querySelector('#listaTarefas');
+let janelaEdicao = document.querySelector('#janelaEdicao');
+let janelaEdicaoFundo = document.querySelector('#janelaEdicaoFundo');
+let janelaEdicaoBtnFechar = document.querySelector('#janelaEdicaoBtnFechar');
+let btnAtualizarTarefa = document.querySelector('#btnAtualizarTarefa');
+let idTarefaEdicao = document.querySelector('#idTarefaEdicao');
+let inputTarefaNomeEdicao = document.querySelector('#inputTarefaNomeEdicao');
+const qtdIdsDisponiveis = Number.MAX_VALUE;
 
-inputNovaTarefa.addEventListener("keypress", (e) => {
-  if (e.keyCode == 13) {
-    //13 o codigo do enter no teclado
-    let task = {
-      name: inputNovaTarefa.value,
-      id: randomId(),
-    };
-    //Add task in the HTML
-    addTask(task);
-  }
+inputNovaTarefa.addEventListener('keypress', (e) => {
+
+    if(e.keyCode == 13) {
+        let tarefa = {
+            nome: inputNovaTarefa.value,
+            id: gerarIdV2(),
+        }
+        adicionarTarefa(tarefa);
+    }
 });
 
-btnAddTarefa.addEventListener("click", (e) => {
-  let task = {
-    name: inputNovaTarefa.value,
-    id: randomId(),
-  };
-  //Add task in the HTML
-  addTask(task);
+janelaEdicaoBtnFechar.addEventListener('click', (e) => {
+    alternarJanelaEdicao();
 });
 
-function randomId() {
-  return Math.floor(Math.random() * 3000);
+btnAddTarefa.addEventListener('click', (e) => {
+
+    let tarefa = {
+        nome: inputNovaTarefa.value,
+        id: gerarIdV2(),
+    }
+    adicionarTarefa(tarefa);
+});
+
+btnAtualizarTarefa.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let idTarefa = idTarefaEdicao.innerHTML.replace('#', '');
+
+    let tarefa = {
+        nome: inputTarefaNomeEdicao.value,
+        id: idTarefa
+    }
+
+    let tarefaAtual = document.getElementById(''+idTarefa+'');
+
+    if(tarefaAtual) {
+        let li = criarTagLI(tarefa);
+        listaTarefas.replaceChild(li, tarefaAtual);
+        alternarJanelaEdicao();
+    } else {
+        alert('Elemento HTML não encontrado!');
+    } 
+});
+
+function gerarId() {
+    return Math.floor(Math.random() * qtdIdsDisponiveis);
 }
 
-function addTask(task) {
-  let li = createTagLi(task);
-  listaTarefa.appendChild(li);
-  inputNovaTarefa.value = '';
+function gerarIdV2() {
+    return gerarIdUnico();
 }
 
-function createTagLi(task) {
-  let li = document.createElement("li");
-  let span = document.createElement("span");
-  span.classList.add('textoTarefa');
-  span.innerHTML = task.name;
+function gerarIdUnico() {
 
-  let div = document.createElement("div");
+    // debugger;
+    let itensDaLista = document.querySelector('#listaTarefas').children;
+    let idsGerados = [];
 
-  let btnEdit = document.createElement("button");
-  btnEdit.classList.add('btnAcao');
-  btnEdit.innerHTML = ' <i class="fa fa-pencil"></i>';
+    for(let i=0;i<itensDaLista.length;i++) {
+        idsGerados.push(itensDaLista[i].id);
+    }
 
-  let btnTrash = document.createElement("button");
-  btnTrash.classList.add('btnAcao');
-  btnTrash.innerHTML = ' <i class="fa fa-trash"></i>';
+    let contadorIds = 0;
+    let id = gerarId();
 
-  div.appendChild(btnEdit);
-  div.appendChild(btnTrash);
+    while(contadorIds <= qtdIdsDisponiveis && 
+        idsGerados.indexOf(id.toString()) > -1) {
+            id = gerarId();
+            contadorIds++;
 
-  li.appendChild(span);
-  li.appendChild(div);
+            if(contadorIds >= qtdIdsDisponiveis) {
+                alert("Oops, ficamos sem IDS :/");
+                throw new Error("Acabou os IDs :/");
+            }
+        }
 
-  return li;
+    return id;
+}
+
+function adicionarTarefa(tarefa) {
+    let li = criarTagLI(tarefa);
+    listaTarefas.appendChild(li);  
+    inputNovaTarefa.value = '';  
+}
+
+function criarTagLI(tarefa) {
+
+    let li = document.createElement('li');
+    li.id = tarefa.id;
+
+    let span = document.createElement('span');
+    span.classList.add('textoTarefa');
+    span.innerHTML = tarefa.nome;
+
+    let div  = document.createElement('div');
+
+    let btnEditar = document.createElement('button');
+    btnEditar.classList.add('btnAcao');
+    btnEditar.innerHTML = '<i class="fa fa-pencil"></i>';
+    btnEditar.setAttribute('onclick', 'editar('+tarefa.id+')');
+    
+    let btnExcluir  = document.createElement('button');
+    btnExcluir.classList.add('btnAcao');
+    btnExcluir.innerHTML = '<i class="fa fa-trash"></i>';
+    btnExcluir.setAttribute('onclick', 'excluir('+tarefa.id+')');
+
+    div.appendChild(btnEditar);
+    div.appendChild(btnExcluir);
+
+    li.appendChild(span);
+    li.appendChild(div);
+    return li;
+}
+
+function editar(idTarefa) {
+    let li = document.getElementById(''+ idTarefa + '');
+    if(li) {
+        idTarefaEdicao.innerHTML = '#' + idTarefa;
+        inputTarefaNomeEdicao.value = li.innerText;
+        alternarJanelaEdicao();
+    } else {
+        alert('Elemento HTML não encontrado!');
+    }
+}
+
+function excluir(idTarefa) {
+    let confirmacao = window.confirm('Tem certeza que deseja excluir? ');
+    if(confirmacao) {
+        let li = document.getElementById(''+ idTarefa + '');
+        if(li) {
+            listaTarefas.removeChild(li);
+        } else {
+            alert('Elemento HTML não encontrado!');
+        }
+    }
+}
+
+function alternarJanelaEdicao() {
+    janelaEdicao.classList.toggle('abrir');
+    janelaEdicaoFundo.classList.toggle('abrir');
 }
